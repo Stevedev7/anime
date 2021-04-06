396fe9fa4a9afa8189e55dc51fa1e8a77327dc20
Steve Pinto <pinto_steve@yahoo.com>
2021-04-06 13:44:49 +0530

    Save to file

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const puppeteer = require('puppeteer');
-
+const fs = require('fs');
+const url = 'https://myanimelist.net/anime/39783/5-toubun_no_Hanayome_%E2%88%AC/episode';
 (async () => {
-    const browser = await puppeteer.launch({headless : false}); //browser initiate
+    const browser = await puppeteer.launch({headless : true}); //browser initiate
     const page = await browser.newPage();  // opening a new blank page
-    await page.goto('https://myanimelist.net/anime/38101/5-toubun_no_Hanayome/episode', {waitUntil : 'networkidle0'})
+    await page.goto(url, {waitUntil : 'networkidle0'})
     const title = await (await page.$('div div.h1.edit-info div.h1-title div p')).evaluate(node => node.innerText);
-    console.log(title);
 
     const episodeTitileElements = await page.$$('td.episode-title a.fl-l.fw-b');
-    const episodeTitiles = [];
-    await episodeTitileElements.forEach(tag => {
+    await episodeTitileElements.forEach((tag, index)=> {
 
         let name = tag.evaluate(node => node.innerText)
-        name.then(i => console.log(i))
+        name.then(i => {
+            fs.appendFile(`${title}.md`, `Episode ${index + 1} - ${i}\n`, function (err) {
+                if (err) throw err;
+            })
+        })
     })
+    fs.appendFile('.gitignore', `\n${title}.md`, function (err) {
+        if (err) throw err;
+    })
+    
     browser.close();
 })();
