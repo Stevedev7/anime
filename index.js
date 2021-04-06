b9ac6709dd9cce651370da0f15747601e715796a
Steve Pinto <pinto_steve@yahoo.com>
2021-04-06 14:04:41 +0530

    Handle repeated scraping

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const url = 'https://myanimelist.net/anime/39783/5-toubun_no_Hanayome_%E2%88%AC/episode';
+const { Console } = require('console');
+const url = 'https://myanimelist.net/anime/14813/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru/episode';
 (async () => {
-    const browser = await puppeteer.launch({headless : true}); //browser initiate
+    const browser = await puppeteer.launch({headless : false}); //browser initiate
     const page = await browser.newPage();  // opening a new blank page
     await page.goto(url, {waitUntil : 'networkidle0'})
     const title = await (await page.$('div div.h1.edit-info div.h1-title div p')).evaluate(node => node.innerText);
+    try{
+        if(fs.existsSync(`${title}.md`)){
+            console.log('Already Scraped');
+        } else{
+            const episodeTitileElements = await page.$$('td.episode-title a.fl-l.fw-b');
+            await episodeTitileElements.forEach((tag, index)=> {
 
-    const episodeTitileElements = await page.$$('td.episode-title a.fl-l.fw-b');
-    await episodeTitileElements.forEach((tag, index)=> {
-
-        let name = tag.evaluate(node => node.innerText)
-        name.then(i => {
-            fs.appendFile(`${title}.md`, `Episode ${index + 1} - ${i}\n`, function (err) {
+                let name = tag.evaluate(node => node.innerText)
+                name.then(i => {
+                    fs.appendFile(`${title}.md`, `Episode ${index + 1} - ${i}\n`, function (err) {
+                        if (err) throw err;
+                    })
+                })
+            })
+            fs.appendFile('.gitignore', `\n${title}.md`, function (err) {
                 if (err) throw err;
             })
-        })
-    })
-    fs.appendFile('.gitignore', `\n${title}.md`, function (err) {
-        if (err) throw err;
-    })
-    
+        }
+    } catch(e){
+        console.log(e);
+        
+    }
     browser.close();
 })();
